5c48b9b8e2ae8e93dd7f69761a7095b06c95d6f6
Kara Barleta <karabarleta@gmail.com>
2018-11-01 20:03:49 -0400

    Post a job button 2 (#7)
    * add post a job button, add react router, add material ui library
    
    * add .gitignore
    
    * remove logo from App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import StyledButton from './components/common/StyledButton';
+
+const Index = () => <h2>Post a new job</h2>;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <StyledButton component={Link} to="/jobs/post-a-job" color="primary">
+            Post a Job
+          </StyledButton>
+
+          <Route path="/jobs/post-a-job" exact component={Index} />
+        </div>
+      </Router>
     );
   }
 }
