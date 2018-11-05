b5fc076934cffb43208a63be62ab028c3a37c6cf
Kara Barleta <karabarleta@gmail.com>
2018-11-05 17:46:15 -0500

    Feature (#11)
    * Post a job button 2 (#7)
    
    * add post a job button, add react router, add material ui library
    
    * add .gitignore
    
    * remove logo from App.js
    
    * data structure (#8)
    
    * Add navigation bar, assets folder and react-redux dependancies
    
    * Comment out unused variables
    
    * Comment out unused variables in App.js
    
    * Add Job Post form components
    
    * Add Job Post form components and remove unused variables to prevent circleCI error
    
    * update karabarley feature branch
    
    * update JobInfo TextField to outlined, add JobForm cards, add some basic styling to create-job page
    
    * Remove unused variables to ahere to linting standards

---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,61 @@
+// NPM
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Grid from '@material-ui/core/Grid';
+// import Hidden from '@material-ui/core/Hidden';
+// import { connect } from 'react-redux';
+// import PropTypes from 'prop-types';
+
+// App
+import Logo from '../../assets/images/logo_women_and_color.svg';
+// import StyledButton from '../common/StyledButton';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    backgroundColor: theme.palette.primary.contrastText,
+    color: theme.palette.secondary.dark,
+  }
+});
+
+class Navigation extends Component {
+
+  render() {
+    const {
+      classes,
+    } = this.props;
+
+    return (
+      <div>
+        <AppBar position="fixed" className={classes.root}>
+          <Toolbar>
+            <Grid container justify="center" spacing={0}>
+              <Grid item xs={12} md={9}>
+                <Grid container justify="space-between" alignItems="center">
+                  <Grid item xs={6} sm={4} md={3}>
+                    <a
+                      href="/"
+                      style={{ textDecoration: 'none' }}
+                    >
+                      <img src={Logo} height="50px" width="100%" alt={'Women and Color logo'}/>
+                    </a>
+                  </Grid>
+                   <Grid item xs={6} md={4}>
+                    <Grid container justify="flex-end">
+                    {/* Add Styled Button here - may need to bring in Button Menu*/}
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+};
+
+// export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(Navigation));
+export default withStyles(styles)(Navigation);
---
+++ src/pages/create-job/JobForm.js
@@ -0,0 +1,73 @@
+// NPM
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+// import { connect } from 'react-redux';
+// import TextField from '@material-ui/core/TextField';
+// import Input from '@material-ui/core/Input';
+// import InputLabel from '@material-ui/core/InputLabel';
+// import MenuItem from '@material-ui/core/MenuItem';
+// import Checkbox from '@material-ui/core/Checkbox';
+// import FormGroup from '@material-ui/core/FormGroup';
+// import FormLabel from '@material-ui/core/FormLabel';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import ReactLoading from 'react-loading';
+
+// App
+import StyledButton from '../../components/common/StyledButton';
+import FormField from '../../components/common/FormField';
+import EmployeeBenefits from './EmployeeBenefits';
+import JobInfo from './JobInfo';
+
+// import ImageUpload from 'appCommon/ImageUpload';
+// import DefaultLayout from '../../components/layouts/Default';
+// import css from '../../assets/onboarding.module.css';
+
+const styles = theme => ({
+  createJob: {
+    background: '#EFEEEA'
+  },
+  card: {
+    borderRadius: '8px',
+    border: `1px solid #e0e0e0`,
+    margin: '25px auto',
+    padding: '25px',
+    maxWidth: '1000px'
+
+  },
+  jobHeader: {
+    maxWidth: '1000px',
+    margin: 'auto',
+  },
+  adjacentButtons: {
+    display: 'block'
+  }
+})
+
+const JobForm = ({ handleSubmit, classes}) => {
+  return (
+    <form onSubmit={handleSubmit} className={classes.createJob}>
+        <h1 className={classes.jobHeader}>Post a job</h1>
+        <Card elevation={0} className={classes.card}>
+          <JobInfo />
+        </Card>
+        
+        <Card elevation={0} className={classes.card}>
+          <EmployeeBenefits />
+        </Card>
+
+        <Card elevation={0} className={classes.card}>
+          <FormField className={classes.adjacentButtons}>
+            <StyledButton label="Submit" type="submit" color="primary">
+              Post a Job
+          </StyledButton>
+            <StyledButton label="Clear" type="submit" color="secondary">
+              Clear All
+          </StyledButton>
+          </FormField>
+        </Card>
+      </form>
+  );
+};
+
+export default withStyles(styles)(JobForm);
