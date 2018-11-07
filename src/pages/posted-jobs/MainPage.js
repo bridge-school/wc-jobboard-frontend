8ca17d0515d1c81d8c16f50f9d3a5b3191157360
Gala <GalkaS@users.noreply.github.com>
2018-11-06 20:43:17 -0500

    Background image (#13)
    * Feature (#11)
    
    * Post a job button 2 (#7)
    
    * add post a job button, add react router, add material ui library
    
    * add .gitignore
    
    * remove logo from App.js
    
    * data structure (#8)
    
    * Add navigation bar, assets folder and react-redux dependancies
    
    * Comment out unused variables
    
    * Comment out unused variables in App.js
    
    * Add Job Post form components
    
    * Add Job Post form components and remove unused variables to prevent circleCI error
    
    * update karabarley feature branch
    
    * update JobInfo TextField to outlined, add JobForm cards, add some basic styling to create-job page
    
    * Remove unused variables to ahere to linting standards
    
    * Add WC header, update buttons paths
    
    * remove unused variables
    
    * remove unused variables
    
    * Add the background image and header text
    
    * Resolve linting errors
    
    * Fix stylings issue

--- src/pages/posted-jobs/MainPage.js
+++ src/pages/posted-jobs/MainPage.js
@@ -1,13 +1,18 @@
 import React from 'react';
 // import { withStyles } from '@material-ui/core/styles';
 import Navigation from '../../components/navigation/Navigation';
-// import Header from '../../components/header/Header';
+import Header from '../../components/header/Header';
 
 const MainPage = (props) => {
 	return (
+		<div>
 			<div>
 				<Navigation path={props.path} color={props.color} title={props.title}></Navigation>
 			</div>
+			<div>
+				<Header></Header>
+			</div>
+		</div>
 	)
 }
 
