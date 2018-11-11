ead71b324508c003bd31169eff4c4b97119e5c0c
karabarley <karabarleta@gmail.com>
2018-11-11 15:47:40 -0500

    The post-fetch and fetch work here

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,9 @@
 // base API URL
 const API_BASE_URL = process.env.NODE_ENV === 'production' ? 'http://wc-jobboard-backend.bridgeschoolapp.io' : '';
-// const API_BASE_URL_POST = process.env.NODE_ENV === 'production' ? '' : 'http://wc-jobboard-backend.bridgeschoolapp.io';
 
 // example API method
 export const checkApiServer = () =>
+    console.log('fetch data')
     fetch(`${API_BASE_URL}/jobs`)
         .then((res) => res.json())
         .then((response) => {
@@ -15,12 +15,12 @@ export const checkApiServer = () =>
 
 export const postFormObject = ( values) => {
     console.log('post:' , values)
-    fetch(`${API_BASE_URL}/jobs/post-a-job`, {
+    fetch(`${API_BASE_URL}/jobs`, {
         method: 'post',
         headers: {
             "Content-type": "application/json"
         },
-        body: JSON.stringify(values)
+        body: JSON.stringify(values) 
         })
         .then(function (data) {
         console.log('Request succeeded with JSON response', data);
--- src/pages/create-job/JobForm.js
+++ src/pages/create-job/JobForm.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import { postFormObject } from '../../api/index';
+// const  { DOM: { input, select, textarea } } = React
 // import Card from '@material-ui/core/Card';
 // import { connect } from 'react-redux';
 // import TextField from '@material-ui/core/TextField';
@@ -63,6 +64,11 @@ const JobFormComponent = ({ handleSubmit, classes}) => {
       <div>
         <label htmlFor="firstName">First Name</label>
         <Field name="firstName" component="input" type="text" />
+        <label htmlFor="firstName">Last Name</label>
+        <Field name="LastName" component="input" type="text" />
+        <div>
+          <Field name="benefits" id="employed" component="input" type="checkbox"/>
+        </div>
       </div>
       <button type="submit">
         Submit
