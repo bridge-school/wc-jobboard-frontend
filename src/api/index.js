839388ae6e1a618168f2d99b280632d79594c1d9
Kara Barleta <karabarleta@gmail.com>
2018-11-11 16:19:40 -0500

    Update post-fetch and fetch functionality (#18)
    * Feature (#11)
    
    * Post a job button 2 (#7)
    
    * add post a job button, add react router, add material ui library
    
    * add .gitignore
    
    * remove logo from App.js
    
    * data structure (#8)
    
    * Add navigation bar, assets folder and react-redux dependancies
    
    * Comment out unused variables
    
    * Comment out unused variables in App.js
    
    * Add Job Post form components
    
    * Add Job Post form components and remove unused variables to prevent circleCI error
    
    * update karabarley feature branch
    
    * update JobInfo TextField to outlined, add JobForm cards, add some basic styling to create-job page
    
    * Remove unused variables to ahere to linting standards
    
    * Fix CircleCi error in MainPage.js
    
    * Fix circle ci errors
    
    * add post-fetch branch
    
    * send post-fetch values
    
    * The post-fetch and fetch work here

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,11 @@
 // base API URL
-// const API_BASE_URL = process.env.NODE_ENV === 'production' ? '' : 'http://wc-jobboard-backend.bridgeschoolapp.io';
-const API_BASE_URL = 'http://localhost:8081'
+
+const API_BASE_URL = process.env.NODE_ENV === 'production' ? 'http://wc-jobboard-backend.bridgeschoolapp.io' : '';
 
 // example API method
 export const checkApiServer = () =>
+    console.log('fetch data')
+
     fetch(`${API_BASE_URL}/jobs`)
         .then((res) => res.json())
         .then((response) => {
@@ -11,4 +13,22 @@ export const checkApiServer = () =>
         })
         .catch((err) => {
             console.log(err);
+
         });
+
+export const postFormObject = ( values) => {
+    console.log('post:' , values)
+    fetch(`${API_BASE_URL}/jobs`, {
+        method: 'post',
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(values) 
+        })
+        .then(function (data) {
+        console.log('Request succeeded with JSON response', data);
+        })
+        .catch(function (error) {
+        console.log('Request failed', error);
+        });
+}
