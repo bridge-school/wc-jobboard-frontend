15901a50edd5c7df4079057828cf9593664ba813
Evelyn Cheung <ev.cheung33@gmail.com>
2018-11-14 20:13:01 -0500

    resolving circleci error

--- src/pages/posted-jobs/jobsReducer.js
+++ src/pages/posted-jobs/jobsReducer.js
@@ -1,6 +1,6 @@
-const initialState = [];
+const initialState = null;
 
-export const jobsReducer = (state = initialState, action)=> {
+export const jobsReducer = (state = initialState, action) => {
     switch(action.type){
         case 'SET_JOBS': 
             return action.payload
