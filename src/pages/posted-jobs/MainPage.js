47723447e311d1aebb0f4495dc4fc3ed832893d5
Gala <GalkaS@users.noreply.github.com>
2018-11-17 10:31:40 -0500

    Filters (#28)
    * WIP
    
    * Refactor checkbox options
    
    * Refactor job filter checkboxes

--- src/pages/posted-jobs/MainPage.js
+++ src/pages/posted-jobs/MainPage.js
@@ -6,12 +6,10 @@ import SearchField from "../../components/navigation/SearchField";
 import JobPostCard from '../../components/common/JobPostCard';
 import '../../assets/header.module.css';
 import Header from '../../components/header/Header';
-import JobFilters from "../../components/common/JobFilters";
-import CheckboxOption from "../../components/common/CheckboxOption";
 import {checkApiServer} from '../../api/index';
-import Checkbox from '@material-ui/core/Checkbox';
+import JobFilterList from "./JobFilterList"
+import JobFilter from "./JobFilter"
 
-import '../../assets/styles.module.css';
 import { setJobs } from './jobs-actions';
 
 const mapDispatchToProps = {
@@ -52,296 +50,86 @@ class MainPage extends Component {
           ))
         }
         </div>
-        <div className="jobFilter">
-        <JobFilters filterTitle="Bathroom">
-          <CheckboxOption
-                  // value="false"
-                  control={<Checkbox color="primary" />}
-                  label="Baby change table"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  // value="false"
-                  control={<Checkbox color="primary" />}
-                  label="Gender neutral"
-                  >
-          </CheckboxOption>
-                  <CheckboxOption
-                  // value="false"
-                  control={<Checkbox color="primary" />}
-                  label="Single stall"
-                  >
-          </CheckboxOption>
-                  <CheckboxOption
-                  // value="false"
-                  control={<Checkbox color="primary" />}
-                  label="Wheelchair accesible stalls"
-                  >
-          </CheckboxOption>
-        </JobFilters>
-        <JobFilters filterTitle="Days off">
-          <CheckboxOption
-                  // value="false"
-                  control={<Checkbox color="primary" />}
-                  label="Health"
-                  >
-          </CheckboxOption>
-                  <CheckboxOption
-                  // value="false"
-                  control={<Checkbox color="primary" />}
-                  label="Personal"
-                  >
-          </CheckboxOption>
-                  <CheckboxOption
-                  // value="false"
-                  control={<Checkbox color="primary" />}
-                  label="Professional development"
-                  >
-          </CheckboxOption>
-        </JobFilters>
-        <JobFilters filterTitle="Employee development">
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Career coaching"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Employee resources"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Hackathons"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Lunch and learns"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Mentorship"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Development budget"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Tuition reimbursement"
-                  >
-          </CheckboxOption>
-        </JobFilters>
-        <JobFilters filterTitle="Employment">
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Flexible hours"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Name blind hiring"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Personalized workstation"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Full transit"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Subsidized transit"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Relocation"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Remote"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Work from home"
-                  >
-          </CheckboxOption>
-        </JobFilters>
-        <JobFilters filterTitle="Family">
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="On site children"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Subsidized childcare"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Parental leave"
-                  >
-          </CheckboxOption>
-        </JobFilters>
-        <JobFilters filterTitle="Finances">
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Stock plan"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Retirement savings"
-                  >
-          </CheckboxOption>
-        </JobFilters>
-        <JobFilters filterTitle="Health">
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Dental"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Employee assistance"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Flexible spending"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Full gym"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Subsidized gym"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Sellness budget"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Health"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Vision"
-                  >
-          </CheckboxOption>
-        </JobFilters>
-        <JobFilters filterTitle="Life">
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Life"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Disability"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Travel"
-                  >
-          </CheckboxOption>
-        </JobFilters>
-        <JobFilters filterTitle="Office">
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Snacks"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Meditation"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Prayer"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Pumping"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Stand up desks"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Accesible"
-                  >
-          </CheckboxOption>
-        </JobFilters>
-        <JobFilters filterTitle="Policy">
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Anti harassment"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Code of conduct"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Parental leave"
-                  >
-          </CheckboxOption>
-        </JobFilters>
-        <JobFilters filterTitle="Social">
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Daily lunch"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Weekly lunch"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Monthly outings"
-                  >
-          </CheckboxOption>
-          <CheckboxOption
-                  control={<Checkbox color="primary" />}
-                  label="Quarterly outings"
-                  >
-          </CheckboxOption>
-        </JobFilters>
+
+      <div className="jobFilter">
+        <JobFilterList filterTitle="Bathroom">
+          <JobFilter label="Baby change table" />
+          <JobFilter label="Gender neutral" />
+          <JobFilter label="Single stall" />
+          <JobFilter label="Wheelchair accesible stalls" />
+        </JobFilterList>
+        <JobFilterList filterTitle="Days off">
+          <JobFilter label="Health" />
+          <JobFilter label="Personal" />
+          <JobFilter label="Professional development" />
+        </JobFilterList>
+        <JobFilterList filterTitle="Employee development">
+          <JobFilter label="Career coaching" />
+          <JobFilter label="Employee resources" />
+          <JobFilter label="Hackathons" />
+          <JobFilter label="Lunch and learns" />
+          <JobFilter label="Mentorship" />
+          <JobFilter label="Development budget" />
+          <JobFilter label="Tuition reimbursement" />
+        </JobFilterList>
+        <JobFilterList filterTitle="Employment">
+          <JobFilter label="Flexible hours" />
+          <JobFilter label="Name blind hiring" />
+          <JobFilter label="Personalized workstation" />
+          <JobFilter label="Full transit" />
+          <JobFilter label="Subsidized transit" />
+          <JobFilter label="Relocation" />
+          <JobFilter label="Remote" />
+          <JobFilter label="Work from home" />
+        </JobFilterList>
+        <JobFilterList filterTitle="Family">
+          <JobFilter label="On site children" />
+          <JobFilter label="Subsidized childcare" />
+          <JobFilter label="Parental leave" />
+        </JobFilterList>
+        <JobFilterList filterTitle="Finances">
+          <JobFilter label="Stock plan" />
+          <JobFilter label="Retirement savings" />
+        </JobFilterList>
+        <JobFilterList filterTitle="Health">
+          <JobFilter label="Dental" />
+          <JobFilter label="Employee assistance" />
+          <JobFilter label="Flexible spending" />
+          <JobFilter label="Full gym" />
+          <JobFilter label="Subsidized gym" />
+          <JobFilter label="Sellness budget" />
+          <JobFilter label="Health" />
+          <JobFilter label="Vision" />
+        </JobFilterList>
+        <JobFilterList filterTitle="Life">
+          <JobFilter label="Life" />
+          <JobFilter label="Disability" />
+          <JobFilter label="Travel" />
+        </JobFilterList>
+        <JobFilterList filterTitle="Office">
+          <JobFilter label="Snacks" />
+          <JobFilter label="Meditation" />
+          <JobFilter label="Prayer" />
+          <JobFilter label="Pumping" />
+          <JobFilter label="Stand up desks" />
+          <JobFilter label="Accesible" />
+        </JobFilterList>
+        <JobFilterList filterTitle="Policy">
+          <JobFilter label="Anti harassment" />
+          <JobFilter label="Code of conduct" />
+          <JobFilter label="Parental leave" />
+        </JobFilterList>
+        <JobFilterList filterTitle="Social">
+          <JobFilter label="Daily lunch" />
+          <JobFilter label="Weekly lunch" />
+          <JobFilter label="Monthly outings" />
+          <JobFilter label="Quarterly outings" />
+        </JobFilterList>
       </div>
       </div>
     </div>
     </React.Fragment>
-  )
+    )
   }
 }
 
